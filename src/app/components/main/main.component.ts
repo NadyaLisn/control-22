55013d6f18903e9dd827e84b1012c2ab7d3c7ee1
Nadya Lisn <nadyalisn@gmail.com>
2025-03-12 15:29:05 +0400

    accordion

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,6 +3,14 @@ import {NgIf} from "@angular/common";
 import {ProductsComponent} from "../products/products.component";
 import {MainPopupComponent} from "../main-popup/main-popup.component";
 import {takeWhile, timer} from "rxjs";
+import {
+  NgbAccordionConfig, NgbAccordionModule,
+  NgbAccordionBody,
+  NgbAccordionButton, NgbAccordionCollapse,
+  NgbAccordionDirective, NgbAccordionHeader,
+  NgbAccordionItem
+} from "@ng-bootstrap/ng-bootstrap";
+import {AccordionComponent} from "../accordion/accordion.component";
 
 // declare let $: any;
 @Component({
@@ -11,7 +19,9 @@ import {takeWhile, timer} from "rxjs";
   imports: [
     ProductsComponent,
     NgIf,
-    MainPopupComponent
+    MainPopupComponent,
+    AccordionComponent,
+
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
@@ -29,6 +39,7 @@ export class MainComponent implements OnInit, OnDestroy {
     //   icons: false
     // });
   }
+
 
   goToCatalog() {
     window.location.href = '/products';
